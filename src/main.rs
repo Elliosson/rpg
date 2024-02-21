186b6fdd184577386320bc040d10b242f506d6d8
elliosson <elliasss@laposte.net>
2024-02-21 01:45:00 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+//! A simplified implementation of the classic game "Breakout".
+
+use bevy::prelude::*;
+
+const PLAYER_SPEED: f32 = 500.0;
+
+#[derive(Component)]
+struct Player;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        // Add our gameplay simulation systems to the fixed timestep schedule
+        // which runs at 64 Hz by default
+        .add_systems(
+            FixedUpdate,
+            (move_player)
+                // `chain`ing systems together runs them in order
+                .chain(),
+        )
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
+}
+
+// Add the game's entities to our world
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                ..default()
+            },
+            ..default()
+        },
+        Player,
+    ));
+}
+
+fn move_player(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    let mut player_transform = query.single_mut();
+    let mut direction_x = 0.0;
+    let mut direction_y = 0.0;
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        direction_x -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        direction_x += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowDown) {
+        direction_y -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
+        direction_y += 1.0;
+    }
+
+    player_transform.translation.x =
+        player_transform.translation.x + direction_x * PLAYER_SPEED * time.delta_seconds();
+    player_transform.translation.y =
+        player_transform.translation.y + direction_y * PLAYER_SPEED * time.delta_seconds();
+}
