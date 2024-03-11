37bf00d147672a4182b91fb0c3b12d79eb92c1a2
elliosson <elliasss@laposte.net>
2024-03-11 23:29:24 +0100

    Change the image of the weapon with the ui

--- src/systems/item_slot_button.rs
+++ src/systems/item_slot_button.rs
@@ -7,18 +7,23 @@ pub fn item_slot_button(
         (&Interaction, &mut BorderColor, &mut UiImage, &SlotButton),
         (Changed<Interaction>, With<Button>),
     >,
+    mut equiped_weapons: Query<
+        (Entity, &mut Handle<Image>),
+        (With<EquipedWeapon>, Without<SlotButton>),
+    >,
 ) {
     let hammer: Handle<Image> = asset_server.load("hammer_icon.png");
     let sword: Handle<Image> = asset_server.load("sword_icon.png");
+    let (_, mut equiped_weapon_image) = equiped_weapons.single_mut();
 
-    for (interaction, mut border_color, mut ui_image, slot_button) in &mut interaction_query {
+    for (interaction, mut border_color, mut _ui_image, slot_button) in &mut interaction_query {
         match *interaction {
             Interaction::Pressed => {
                 if slot_button.id == 1 {
-                    *ui_image = UiImage::new(hammer.clone());
+                    *equiped_weapon_image = hammer.clone();
                 }
                 if slot_button.id == 2 {
-                    *ui_image = UiImage::new(sword.clone());
+                    *equiped_weapon_image = sword.clone();
                 }
                 border_color.0 = Color::RED;
             }
